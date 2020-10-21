d100a1fd086bcad503091c85046dc9b4229ac4cb
Nayla G Escribano <ngarcia217@ikasle.ehu.eus>
2020-10-21 19:11:06 +0200

    Minor improvements in Scorer.scala to compare normalizations.

--- src/main/scala/org/clulab/timenorm/scfg/Scorer.scala
+++ src/main/scala/org/clulab/timenorm/scfg/Scorer.scala
@@ -14,13 +14,9 @@ object Scorer {
 
     // Indicate the names for the gold standard, normalization and error files.
     // Input files shoud be in "{expression}\t{type}\t{normalization}\n" format.
-    val goldOutFile = "%s/es-gold-standard.txt".format(outDir)
-    val normOutFile = "%s/es-normalizations0.txt".format(outDir)
-<<<<<<< HEAD
-    val errorFile = "%s/es-errors.txt".format(outDir)
-=======
-    val errorFile = "%s/es-errors0.txt".format(outDir)
->>>>>>> 17e1d10097ff76885bcc29c80ec4e8dc72bcd345
+    val goldOutFile = "%s/gold-standard.txt".format(outDir)
+    val normOutFile = "%s/normalizations.txt".format(outDir)
+    val errorFile = "%s/errors.txt".format(outDir)
 
     val errorWriter=new PrintWriter(new File(errorFile))
 
@@ -36,19 +32,19 @@ object Scorer {
     var sumNorm = 0
 
     // Compare each pair in the zipped list.
-    for (timexes <- goldList zip normList) {
+    for ( (goldTimex, normTimex) <- goldList zip normList ) {
       sumGold += 1
-      // If both parts of the tuple are the same, sum 1 to sumNorm.
-      if ( timexes._1 == timexes._2 ) {
-        sumNorm += 1
+      val gold = goldTimex.split("\t")
+      val norm = normTimex.split("\t")
+      // In case there is a normalization, this can be correct or incorrect.
+      if (norm.length == 2) {
+        // If both normalizations are the same, sum 1 to sumNorm.
+        if ( gold(1) == norm(1) ) { sumNorm += 1 }
+        // If not, write the error in errorFile.
+        else { errorWriter.write("%s | %s\n".format(goldTimex, norm(1))) }
       }
-      // If not, write the error in errorFile.
-      else {
-        if (timexes._2.split("\t").length == 2) {
-          errorWriter.write("%s | %s\n".format(timexes._1, timexes._2.split("\t")(1)))
-        }
-        else {errorWriter.write("%s\n".format(timexes._1))}
-      }
+      // If there is no normalization, write the timex in errorFile.
+      else { errorWriter.write("%s\n".format(goldTimex)) }
     }
 
     errorWriter.close()
@@ -69,11 +65,7 @@ object Scorer {
     // Consider every gold standard file.
     for (file <- new java.io.File(inDir).listFiles) {
       // Keep and write only the expressions and their normalizations.
-<<<<<<< HEAD
-      val timexList = Source.fromFile(file).getLines.toList.tail
-=======
       val timexList = Source.fromFile(file).getLines.toList
->>>>>>> 17e1d10097ff76885bcc29c80ec4e8dc72bcd345
       for (timex <- timexList) {
         val expression = timex.split("\t").head
         val normalization = timex.split("\t").last
@@ -96,13 +88,8 @@ object Scorer {
     // Consider every file of the directory whose timexes will be normalized.
     for (file <- new java.io.File(inDir).listFiles) {
 
-<<<<<<< HEAD
-      // Get the temporal anchor from the first line of the file.
-      val dctString = Source.fromFile(file).getLines.toList.head
-=======
       // Get the temporal anchor from the normalization of the first line.
       val dctString = Source.fromFile(file).getLines.toList.head.split("\t").last
->>>>>>> 17e1d10097ff76885bcc29c80ec4e8dc72bcd345
 
       val pattern = "T".r
       val anchor = pattern.findFirstIn(dctString) match {
@@ -132,24 +119,20 @@ object Scorer {
       }
 
       // Get and write the normalizations of all the timexes in the file.
-<<<<<<< HEAD
-      val timexList = Source.fromFile(file).getLines.toList.tail
-=======
       val timexList = Source.fromFile(file).getLines.toList
->>>>>>> 17e1d10097ff76885bcc29c80ec4e8dc72bcd345
       for (timex <- timexList) {
         // Get and write the expression and its normalization.
         val expression = timex.split("\t").head
         parser.parse(expression, anchor) match {
           // If the parser fails, write only the expression.
           case Failure(temporal) =>
-            println(expression)
+            println("%s".format(expression))
             normWriter.write("%s\n".format(expression))
           // If the parser successes, write the expression and normalization.
           case Success(temporal) =>
-            val result = temporal.timeMLValue
-            println("%s\t%s".format(expression, result))
-            normWriter.write("%s\t%s\n".format(expression, result))
+            val normalization = temporal.timeMLValue
+            println("%s\t%s".format(expression, normalization))
+            normWriter.write("%s\t%s\n".format(expression, normalization))
         }
       }
     }
